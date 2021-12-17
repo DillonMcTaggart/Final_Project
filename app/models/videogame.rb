9fd883041bb3fbd60adeb258fec1619faa9a72d7
Dillon <dmctaggart@aceproject.space>
2021-12-16 23:54:23 -0600

    edited seeds file

--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -1,6 +1,6 @@
 class Videogame < ApplicationRecord
   belongs_to :customers
-  validates :name, :platform, :genre, :publisher
+  validates :name, :platform, :genre, :publisher, presence: true
   validates :rank, :year, numericality: { only_integer: true }
   validates :global_sales, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Videogame.delete_all
+Customer.delete_all
+
+filename = Rails.root.join('db/vgsales.csv')
+
+puts "Loading videogames from csv file: #{filename}"
+
+csv_data = File.read(filename)
+videogames = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+videogames.each do |v|
+  puts v['Name']
+end
