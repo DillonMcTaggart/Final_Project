41857873b915293e1338e4fd50c07103510e3543
Dillon <dmctaggart@aceproject.space>
2021-12-17 14:49:58 -0600

    created seeds.rb to populate the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,19 @@ csv_data = File.read(filename)
 videogames = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
 
 videogames.each do |v|
-  puts v['Name']
+  custom = Customer.find_or_create_by(name: v['Name'])
+  if custom && custom.valid?
+    videogame = custom.videogames.create(
+      rank: v['Rank'],
+      name: v['Name'],
+      platform: v['Platform'],
+      year: v['Year'],
+      genre: v['Genre'],
+      publisher: v['Publisher'],
+      global_sales: v['Global_Sales']
+    )
+    puts "Invalid movie #{v['Name']}" unless videogame&.valid?
+  else
+    puts "Invalid Customer: #{v['Name']} for videogame: #{v['Name']}"
+  end
 end
